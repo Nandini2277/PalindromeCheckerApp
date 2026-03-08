3aa2abfac54dda6ecb4957e57a6360b5068aef53
Nandini Srivastava <227776554+Nandini2277@users.noreply.github.com>
2026-03-08 21:40:52 +0530

    Use Case 1

---
+++ UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class UseCase1PalindromeCheckerApp {
+    UseCase1PalindromeCheckerApp(){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialised successfully.");
+    }
+    public static void main(String[] args){
+        new UseCase1PalindromeCheckerApp();
+    }
+}
